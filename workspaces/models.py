e2bbbcd13fad020a5244c301bdeab3e55f3fcf29
Navid <njannati1991@gmail.com>
2026-05-15 10:03:19 +0330

    add: models for workspaces app

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from django.conf import settings
+from django.contrib.auth import get_user_model
+from core.models import BaseModel
 
-# Create your models here.
+
+User = get_user_model()
+
+class Workspace(BaseModel):
+
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workspaces')
+    members = models.ManyToManyField(User, related_name='workspaces', blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
