33054778b67c27aaa99dbf3b1b4cad7e3fa82ae8
Navid <njannati1991@gmail.com>
2026-05-15 11:21:53 +0330

    modified: Project.admin and Workspace.admin and requirements file

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'workspace__name')
+    list_display_links = ('id', 'name')
+    list_filter = ('workspace__name', )
+
+    search_fields = ('name', 'workspcase__name')
+
+
+    prepopulated_fields = {'slug': ('name', )}
+    
--- workspaces/admin.py
+++ workspaces/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Workspace
+
+
+
+@admin.register(Workspace)
+class WorkspaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner__username', 'get_member_name')
+    list_display_links = ('id', 'name')
+
+    search_fields = ('name', 'owner__')
+
+
+    def get_member_name(self, obj):
+        return ", ".join([member.username for member in obj.members.all()])
+
+    get_member_name.short_description = 'Members'
