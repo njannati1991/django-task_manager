805a4e41b7f411fe3bfc0e878cdab19adcd203fe
Navid <njannati1991@gmail.com>
2026-05-15 11:43:41 +0330

    add: models for tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from core.models import BaseModel
+from projects.models import Project
+
+User = get_user_model()
+class Task(BaseModel):
+
+    class Status(models.TextChoices):
+        TODO = 'todo', 'To Do'
+        IN_PROGRESS = 'in_progerss', 'In Progerss'
+        DONE = 'done', 'Done'
+
+
+    class Priority(models.TextChoices):
+        LOW = 'low', 'Low'
+        MEDIUM = 'medium', 'Medium'
+        HIGH = 'high', 'High'
+
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tesks')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.TODO)
+    priority = models.CharField(max_length=20, choices=Priority.choices, default=Priority.MEDIUM)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    
+
+
+
